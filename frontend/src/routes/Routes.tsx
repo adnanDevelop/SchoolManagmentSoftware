3e6113d1b454989d0f42ab4db6151fc3298a4e6b
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2025-04-23 18:11:41 +0500

    fixed file import bug

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,77 +1,31 @@
-import { useAppSelector } from "@/redux/store";
+// import { useAppSelector } from "@/redux/store";
 import { Outlet, useRoutes } from "react-router-dom";
 import { ProtectedRoute, PublicRoute } from "@/utils/RootAuth";
 
 // Main Layout
-import Layout from "@/components/layout/Layout";
 
 // Auth pages
-import Login from "@/modules//auth/login/Login";
-import Register from "@/modules//auth/register/Register";
 
 //Main Pages
-import Task from "@/modules/task/Task";
-import Team from "@/modules/team/Team";
-import Dashboard from "@/modules/dashboard";
-import Client from "@/modules/client/Client";
-import Project from "@/modules/project/Project";
-import NotFound from "@/modules//notFound/NotFound";
-import TaskDetail from "@/modules/taskDetail/TaskDetail";
-import ClientDetail from "@/modules/clientDetail/ClientDetail";
-import ProjectDetail from "@/modules/projectDetail/ProjectDetail";
+
+// import { RootState } from "../redux/store";
 
 export const Routes = () => {
-  const { user } = useAppSelector((state) => state.auth);
+  // const { user } = useAppSelector((state: RootState) => state.auth);
 
   return useRoutes([
     {
       path: "/",
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
+      element: <ProtectedRoute>{/* <Layout /> */}</ProtectedRoute>,
       children: [
-        {
-          index: true,
-          element: <Dashboard />,
-        },
-        {
-          path: "/project",
-          element: <Project />,
-        },
-        {
-          path: "/project/:id",
-          element: <ProjectDetail />,
-        },
-        {
-          path: "/task",
-          element: <Task />,
-        },
-        {
-          path: "/task/:id",
-          element: <TaskDetail />,
-        },
-        ...(user.role !== "client"
-          ? [
-              {
-                path: "/client",
-                element: <Client />,
-              },
-            ]
-          : []),
-        ...(user.role !== "client"
-          ? [
-              {
-                path: "/client/:id",
-                element: <ClientDetail />,
-              },
-            ]
-          : []),
-        {
-          path: "/user",
-          element: <Team />,
-        },
+        // {
+        //   index: true,
+        //   element: <Dashboard />,
+        // },
+        // {
+        //   path: "/project",
+        //   element: <Project />,
+        // },
       ],
     },
     {
@@ -81,19 +35,19 @@ export const Routes = () => {
         </PublicRoute>
       ),
       children: [
-        {
-          path: "login",
-          element: <Login />,
-        },
-        {
-          path: "register",
-          element: <Register />,
-        },
+        // {
+        //   path: "login",
+        //   element: <Login />,
+        // },
+        // {
+        //   path: "register",
+        //   element: <Register />,
+        // },
       ],
     },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
+    // {
+    //   path: "*",
+    //   element: <NotFound />,
+    // },
   ]);
 };
